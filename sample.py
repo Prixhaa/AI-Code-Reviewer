7de5d1c7aad0fbdb204ca8866098ec99f15b91a7
Prisha Sharma <sharmaprisha333@gmail.com>
2026-06-25 14:54:06 +0530

    Add sample Python file

---
+++ sample.py
@@ -0,0 +1,16 @@
+# Sample Python file
+
+class Student:
+
+```
+def __init__(self, name):
+    self.name = name
+
+def display(self):
+    print(self.name)
+```
+
+def greet():
+print("Hello World")
+
+# TODO
